aab25d7d19323c3adc62e3f891f6c3757c96bd86
Taavi Hautoniemi <taavihautoniemi@hotmail.com>
2016-01-19 23:04:20 +0200

    Initial commit.

---
+++ T4/T4/Vehicle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4
+{
+    class Vehicle
+    {
+        // properties
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public int Tyres { get; set; }
+
+        public Vehicle(string name)
+        {
+            Name = name;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Car data : ");
+            Console.WriteLine("- name : " + Name);
+            Console.WriteLine("- speed : " + Speed);
+            Console.WriteLine("- tyres : " + Tyres);
+        }
+
+        public override string ToString()
+        {
+            return "Vehicle: " + Name + ", " + Speed + ", " + Tyres;
+        }
+
+    }
+}
