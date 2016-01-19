2f2bc2ed37ddab6fb6da7f88df0b9c34750c6e3d
Taavi Hautoniemi <taavihautoniemi@hotmail.com>
2016-01-19 23:01:13 +0200

    Initial commit.

---
+++ T4/T4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Create vehicle
+            Vehicle audi = new Vehicle("Audi");
+            audi.Speed = 150;
+            audi.Tyres = 4;
+            audi.PrintData();
+            Console.ReadLine();
+
+            // ToString
+            Console.WriteLine(audi);
+            Console.ReadLine();
+
+        }
+    }
+}
